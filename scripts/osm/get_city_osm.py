04293dc5579d581b63f57fe503753f8459380038
yyh <yuanhongyu.me@gmail.com>
2023-11-30 23:31:40 +0800

    优化数据处理

--- scripts/osm/get_city_osm.py
+++ scripts/osm/get_city_osm.py
@@ -3,6 +3,27 @@ import argparse
 import osmnx as ox
 import tqdm
 import os
+import elevation
+from shapely.geometry import Point, box
+
+def create_area_polygon(lat, lon, radius):
+    # 创建代表感兴趣区域的多边形
+    center = Point(lon, lat)
+    
+    area_polygon = box(center.x - radius, center.y - radius, center.x + radius, center.y + radius)
+    
+    return area_polygon
+
+def get_dem(lat, lon, radius, output_path):
+    # 设置DEM数据的边界
+    bounds = (lon-radius, lat-radius, lon+radius, lat+radius)
+    elevation.clip(bounds=bounds, output=output_path)
+
+def clip_gdf_to_area(gdf, area_polygon):
+    
+    clipped_gdf = gdf.copy()
+    clipped_gdf['geometry'] = gdf['geometry'].intersection(area_polygon)
+    return clipped_gdf
 
 
 if __name__ == '__main__':
@@ -28,36 +49,72 @@ if __name__ == '__main__':
     print(f"{len(citys)} citie's locations loaded.")
     
     for city in tqdm.tqdm(citys.keys(), desc='Processing cities'):
+        
+        try:
 
-        lat, lon = citys[city]
-        # 提取道路网络
-        # road_graph = ox.graph_from_point((lat, lon), network_type='all')
-        road_graph = ox.features_from_point((lat, lon), dist=args.radius, tags={'highway': True})
-        # 提取土地利用数据
-        landuse_query = {'landuse': True, 'natural': True, 'leisure': True, 'waterway': True, 'water': True}
-        landuse_data = ox.features_from_point((lat, lon), dist=args.radius, tags=landuse_query)
+            if not os.path.exists(os.path.join(args.output_dir, city)):
+                os.mkdir(os.path.join(args.output_dir, city))
+            else:
+                continue
+            city_out_path = os.path.join(args.output_dir, city)
+            lat, lon = citys[city]
+            area_polygon = create_area_polygon(lat, lon, 0.00898)
 
-        # 提取建筑物数据
-        buildings_query = {'building': True}
-        buildings_data = ox.features_from_point((lat, lon), dist=args.radius, tags=buildings_query)
-
-
-        # 道路数据转换为GeoDataFrame并保存
-        problematic_cols = [col for col in road_graph.columns if any(isinstance(item, list) for item in road_graph[col])]
-        road_graph = road_graph.drop(columns=problematic_cols)
-        road_graph.to_file("road_data.geojson", driver='GeoJSON')
-
-
-        problematic_cols = [col for col in landuse_data.columns if any(isinstance(item, list) for item in landuse_data[col])]
-        landuse_data = landuse_data.drop(columns=problematic_cols)
-        landuse_data.to_file("landuse_data.geojson", driver='GeoJSON')
-
-        problematic_cols = [col for col in buildings_data.columns if any(isinstance(item, list) for item in buildings_data[col])]
-        buildings_data = buildings_data.drop(columns=problematic_cols)
-        buildings_data.to_file("buildings_data.geojson", driver='GeoJSON')
+            
+            # 提取道路网络
+            # road_graph = ox.graph_from_point((lat, lon), network_type='all')
+            road_graph = ox.features_from_point((lat, lon), dist=args.radius, tags={'highway': True})
+            # road_graph = ox.features_from_polygon(area_polygon, tags={'highway': True})
+            road_types = ['motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'residential', 'service', 'motorway_link', 'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link']
+            road_graph = road_graph[(road_graph['highway'] == 'primary') | (road_graph['highway'] == 'secondary') | (road_graph['highway'] == 'tertiary') | (road_graph['highway'] == 'residential') | (road_graph['highway'] == 'service') ]#  | (road_graph['highway'] == 'motorway') | (road_graph['highway'] == 'trunk')
+            road_graph = clip_gdf_to_area(road_graph, area_polygon)
 
 
-        print(f"OSM data for {city} saved.")
+            landuse_query = {'landuse': True}
+            landuse_data = ox.features_from_point((lat, lon), dist=args.radius, tags=landuse_query)
+            # landuse_data = ox.features_from_polygon(area_polygon, tags=landuse_query)
+            landuse_data = clip_gdf_to_area(landuse_data, area_polygon)
+
+
+            nature_query = {'natural': True, 'water': True, 'waterway': True}
+            nature_data = ox.features_from_point((lat, lon), dist=args.radius, tags=nature_query)
+            # nature_data = ox.features_from_polygon(area_polygon, tags=nature_query)
+            nature_data = clip_gdf_to_area(nature_data, area_polygon)
+
+
+            buildings_query = {'building': True}
+            buildings_data = ox.features_from_point((lat, lon), dist=args.radius, tags=buildings_query)
+            # buildings_data = ox.features_from_polygon(area_polygon, tags=buildings_query)
+            buildings_data = clip_gdf_to_area(buildings_data, area_polygon)
+
+
+
+            problematic_cols = [col for col in road_graph.columns if any(isinstance(item, list) for item in road_graph[col])]
+            road_graph = road_graph.drop(columns=problematic_cols)
+            road_graph.to_file(os.path.join(city_out_path, "road_data.geojson"), driver='GeoJSON')
+            
+
+
+            problematic_cols = [col for col in landuse_data.columns if any(isinstance(item, list) for item in landuse_data[col])]
+            landuse_data = landuse_data.drop(columns=problematic_cols)
+            landuse_data.to_file(os.path.join(city_out_path, "landuse_data.geojson"), driver='GeoJSON')
+
+
+            problematic_cols = [col for col in buildings_data.columns if any(isinstance(item, list) for item in buildings_data[col])]
+            buildings_data = buildings_data.drop(columns=problematic_cols)
+            buildings_data.to_file(os.path.join(city_out_path, "buildings_data.geojson"), driver='GeoJSON')
+
+
+            problematic_cols = [col for col in nature_data.columns if any(isinstance(item, list) for item in nature_data[col])]
+            nature_data = nature_data.drop(columns=problematic_cols)
+            nature_data.to_file(os.path.join(city_out_path, "nature_data.geojson"), driver='GeoJSON')
+
+
+
+            print(f"OSM data for {city} saved.")
+        except Exception as e:
+            print(f"Error occured when processing {city}: {e}")
+            continue
 
     
 
--- scripts/osm/preprocess.py
+++ scripts/osm/preprocess.py
@@ -1,42 +1,78 @@
 import argparse
 import osmnx as ox
-
-
+import os
+import tqdm
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-def plot_and_save_geojson(file_name, output_image_name, plot_type, xlim=None, ylim=None):
-    gdf = gpd.read_file(file_name)
-    if plot_type == 'landuse':
-        gdf['landuse_sum'] = gdf['landuse'].fillna('') + '_' + gdf['leisure'].fillna('') + '_' + gdf['natural'].fillna('') + '_' + gdf['water'].fillna('') + '_' + gdf['waterway'].fillna('')
-    fig, ax = plt.subplots()
+def plot_and_save_geojson(file_name, out_root, plot_type, xlim=None, ylim=None, fig_size=(10, 10)):
+    try:
+        gdf = gpd.read_file(file_name)
 
-    if plot_type == 'landuse':
         
-        gdf.plot(ax=ax, column='landuse_sum', cmap='Set3', legend=True)
-
-
-    elif plot_type == 'building':
-        # 位置图
-        gdf.plot(ax=ax, color='grey')
+        
+        fig, ax = plt.subplots(figsize=fig_size)
         ax.axis('off')
-        if xlim and ylim:
-            ax.set_xlim(xlim)
-            ax.set_ylim(ylim)
-        fig.savefig(f'{output_image_name}_location.jpg', dpi=300, bbox_inches='tight', format='jpg')
 
-        # 高度图
-        gdf.plot(ax=ax, column='height', cmap='grey')  # 根据建筑高度着色
+        if plot_type == 'landuse':
+            gdf.plot(ax=ax, column='landuse', cmap='Set3', legend=True)
+            fig.savefig(os.path.join(out_root, 'landuse.jpg'), bbox_inches='tight', format='jpg')
 
-    elif plot_type == 'road':
-        gdf.plot(ax=ax, column='highway', cmap='tab20')  # 根据道路类型着色
+        elif plot_type == 'building':
 
-    ax.axis('off')
-    if xlim and ylim:
-        ax.set_xlim(xlim)
-        ax.set_ylim(ylim)
+            # 位置图
+            gdf.plot(ax=ax, color='grey')
+            if xlim and ylim:
+                ax.set_xlim(xlim)
+                ax.set_ylim(ylim)
+            fig.savefig(os.path.join(out_root, 'building_location.jpg'), bbox_inches='tight', format='jpg')
 
-    fig.savefig(f'{output_image_name}.jpg', dpi=300, bbox_inches='tight', format='jpg')
+            # 清除当前的ax和fig，为高度图重新创建
+            plt.clf()
+            fig, ax = plt.subplots(figsize=fig_size)
+            ax.axis('off')
+
+
+            # 判断'height'字段是否存在
+            if 'height' not in gdf.columns:
+                raise KeyError("Column 'height' not found in the data.")
+
+            
+            # 高度图
+            gdf.plot(ax=ax, column='height', cmap='viridis')
+            fig.savefig(os.path.join(out_root, 'building_height.jpg'), bbox_inches='tight', format='jpg')
+
+        elif plot_type == 'road':
+            # 判断'highway'字段是否存在
+            if 'highway' not in gdf.columns:
+                raise KeyError("Column 'highway' not found in the data.")
+
+            gdf.plot(ax=ax, column='highway', cmap='tab20', legend=True)
+            fig.savefig(os.path.join(out_root, 'road.jpg'), bbox_inches='tight', format='jpg')
+
+        elif plot_type == 'nature':
+            # 创建组合列，仅当相关列存在时
+            nature_cols = ['natural', 'water', 'waterway']
+            for col in nature_cols:
+                if col in gdf.columns:
+                    gdf[col] = gdf[col].fillna('')
+                else:
+                    gdf[col] = ''
+            gdf['nature_sum'] = gdf[nature_cols].agg('_'.join, axis=1)
+
+            fig, ax = plt.subplots(figsize=fig_size)
+            ax.axis('off')
+            gdf.plot(ax=ax, column='nature_sum', cmap='Set3', legend=True)
+            fig.savefig(os.path.join(out_root, 'nature.jpg'), bbox_inches='tight', format='jpg')
+
+    except Exception as e:
+        # 输出错误信息到baddata.txt
+        with open(os.path.join(out_root, 'baddata.txt'), 'w') as file:
+            file.write(str(e))
+        
+
+
+    
 
 
 
@@ -45,12 +81,28 @@ def plot_and_save_geojson(file_name, output_image_name, plot_type, xlim=None, yl
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--input', type=str, required=True, help='Input file')
-    # parser.add_argument('--output', type=str, required=True, help='Output file')
-    plot_and_save_geojson('road_data.geojson', 'road_data', 'road')
-    plot_and_save_geojson('landuse_data.geojson', 'landuse_data', 'landuse')
-    plot_and_save_geojson('buildings_data.geojson', 'buildings_data', 'building')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, required=True, help='Input file')
+    parser.add_argument('--output', type=str, required=True, help='Output file')
+
+    args = parser.parse_args()
+    root_path = args.input
+
+    citys = os.listdir(root_path)
+
+    for city in tqdm.tqdm(citys):
+
+        save_path = os.path.join(args.input, city)
+        print(save_path)
+        # 提取道路网络
+        plot_and_save_geojson(os.path.join(save_path, "road_data.geojson"), os.path.join(args.output, city), 'road')
+        # 提取土地利用数据
+        plot_and_save_geojson(os.path.join(save_path, "landuse_data.geojson"), os.path.join(args.output, city), 'landuse')
+        # 提取建筑物数据
+        plot_and_save_geojson(os.path.join(save_path, "buildings_data.geojson"), os.path.join(args.output, city), 'building')
+
+        # 提取自然地物数据
+        plot_and_save_geojson(os.path.join(save_path, "nature_data.geojson"), os.path.join(args.output, city), 'nature')
 
 
 
