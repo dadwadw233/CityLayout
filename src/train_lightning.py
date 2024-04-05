fix pretrain model precision incompatible bug

src/train_lightning.py
@@ -17,7 +17,7 @@ console = Console()
 import warnings
 import torch
 warnings.simplefilter('ignore')
-torch.set_float32_matmul_precision('medium')
+
 def handle(config: DictConfig):
     try:
         